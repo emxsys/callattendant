66c49e6d435fd0976a79d92846d305105b5f6e26
Bruce Schubert <bruce@emxsys.com>
2020-08-05 13:40:36 -0700

    Updated service scripts to provide feedback

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+# Start the Call Attendant service
 sudo systemctl start callattendant.service
+sudo systemctl status callattendant.service
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Start the Call Attendant service
 
 sudo systemctl stop callattendant.service
+sudo systemctl status callattendant.service
