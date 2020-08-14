def3db071570cdd40f64b28179e87b6f3cb5cda7
Bruce Schubert <bruce@emxsys.com>
2020-08-14 03:33:28 -0700

    Added MessageIndicator - Refactored indicators and added a PWMLED version used for the message indicator - Re: #55

--- src/hardware/indicators.py
+++ src/hardware/indicators.py
@@ -23,41 +23,122 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-from gpiozero import LED
+# See: https://gpiozero.readthedocs.io/en/stable/
+# See: https://gpiozero.readthedocs.io/en/stable/api_output.html#led
+from gpiozero import LED, PWMLED
+from pprint import pprint
+import time
+
+GPIO_RING = 2       # pin 3
+GPIO_APPROVED = 3   # pin 5
+GPIO_BLOCKED = 4    # pin 7
+GPIO_MESSAGE = 5    # pin 27
 
 
-def turn_all_off():
-    pass
-
-
-class RingIndicator(object):
+class LEDIndicator(object):
     def turn_on(self):
-        self.led.blink(0.5, 0.2, 10)
+        self.led.on()
+
+    def blink(self, max_times=10):
+        # blink in a separate thread
+        self.led.blink(0.5, 0.2, max_times)
 
     def turn_off(self):
-        pass
+        self.led.off()
 
-    def __init__(self):
-        self.led = LED(2)
+    def __init__(self, gpio_pin):
+        self.led = LED(gpio_pin)
 
 
-class ApprovedIndicator(object):
+class PWMLEDIndicator(LEDIndicator):
     def turn_on(self):
-        self.led.blink(0.5, 0.2, 10)
+        self.led.on()
+
+    def blink(self, max_times=10):
+        # blink in a separate thread
+        self.led.pulse(n=max_times)
 
     def turn_off(self):
-        pass
+        self.led.off()
 
-    def __init__(self):
-        self.led = LED(3)
+    def __init__(self, gpio_pin):
+        self.led = PWMLED(gpio_pin)
 
 
-class BlockedIndicator(object):
-    def turn_on(self):
-        self.led.blink(0.5, 0.2, 10)
+class RingIndicator(LEDIndicator):
 
-    def turn_off(self):
-        pass
+    def __init__(self, gpio_pin=GPIO_RING):
+        LEDIndicator.__init__(self, gpio_pin)
 
-    def __init__(self):
-        self.led = LED(4)
+
+class ApprovedIndicator(LEDIndicator):
+
+    def __init__(self, gpio_pin=GPIO_APPROVED):
+        LEDIndicator.__init__(self, gpio_pin)
+
+
+class BlockedIndicator(LEDIndicator):
+
+    def __init__(self, gpio_pin=GPIO_BLOCKED):
+        LEDIndicator.__init__(self, gpio_pin)
+
+
+class MessageIndicator(PWMLEDIndicator):
+
+    def __init__(self, gpio_pin=GPIO_MESSAGE):
+        PWMLEDIndicator.__init__(self, gpio_pin)
+
+
+def test():
+    """ Unit Tests """
+    import os
+
+    print("*** Running Indicator Unit Tests ***")
+
+    try:
+        print("[Constructing Indicators]")
+        ring = RingIndicator()
+        approved = ApprovedIndicator()
+        blocked = BlockedIndicator()
+        message = MessageIndicator()
+
+        print("[Visual Tests]")
+
+        print("Turning on all LEDs for 5 seconds...")
+        ring.turn_on()
+        approved.turn_on()
+        blocked.turn_on()
+        message.turn_on()
+        time.sleep(5)
+
+        print("Blinking on all LEDs for 5 seconds...")
+        ring.blink()
+        time.sleep(.1)
+        approved.blink()
+        time.sleep(.1)
+        blocked.blink()
+        time.sleep(.1)
+        message.blink()
+        time.sleep(5)
+
+        print("Turning off all LEDs...")
+        ring.turn_off()
+        approved.turn_off()
+        blocked.turn_off()
+        message.turn_off()
+        time.sleep(5)
+
+    except Exception as e:
+        print("*** Unit test FAILED ***")
+        pprint(e)
+        return 1
+
+    print("*** Unit tests PASSED ***")
+    return 0
+
+
+if __name__ == '__main__':
+    """ Run the Unit Tests """
+    import sys
+    # Run the tests
+    sys.exit(test())
