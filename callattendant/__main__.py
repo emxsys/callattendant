08c006c145c438adc3f0bb45d701a0fa41f0111c
Bruce Schubert <bruce@emxsys.com>
2020-08-21 11:02:04 -0700

    Install-generated 'callattendent' script invokes __main__.main - Re: #37

--- callattendant/__main__.py
+++ callattendant/__main__.py
@@ -5,13 +5,19 @@
 #
 #  Copyright 2020 Bruce Schubert <bruce@emxsys.com>
 
+import os
+import sys
 
-if __name__ == '__main__':
-    import os
-    import sys
+def main():
 
+    # Ensure the top-level package is on the path
     currentdir = os.path.dirname(os.path.realpath(__file__))
     sys.path.append(currentdir)
 
+    # Launch the app with the command line args.
     from app import main
     sys.exit(main(sys.argv))
+
+if __name__ == '__main__':
+
+    sys.exit(main())
