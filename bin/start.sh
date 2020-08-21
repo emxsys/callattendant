07ba3398b452bdd12873231d826b309677be6024
Bruce Schubert <bruce@emxsys.com>
2020-08-21 04:44:59 -0700

    Added comment

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start the Call Attendant service
-
+# See the unit file: /lib/systemd/system/callattendant.service
 sudo systemctl start callattendant.service
 sudo systemctl status callattendant.service
